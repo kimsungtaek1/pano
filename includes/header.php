0210fde3157512f1f1a736a8a046cf58e50f4cb1
kimsungtaek1 <kstbook@naver.com>
2025-11-12 20:37:34 +0900

    database.sql 샘플 뉴스 데이터 한국어 문법 수정

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PANO - 법률사무소</title>
+
+    <!-- Open Graph 메타 태그 (카카오톡, 페이스북 등 SNS 공유용) -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="PANO - 법률사무소">
+    <meta property="og:description" content="의뢰인의 믿음과 신뢰를 받을 수 있도록 최선을 다하겠습니다. 40년 전통의 법률사무소">
+    <meta property="og:image" content="https://panolawyer.com/images/thumbnail.png">
+    <meta property="og:url" content="https://panolawyer.com">
+
     <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="stylesheet" href="/css/style.css">
 </head>
