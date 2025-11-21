86ff3b276054339ae9e2453dee5c153fec155505
kimsungtaek1 <kstbook@naver.com>
2025-11-21 21:08:18 +0900

    인덱스 페이지 모바일에서만 '해결까지' 다음에 줄바꿈 추가

--- index.php
+++ index.php
@@ -247,7 +247,7 @@ include 'includes/db.php';
                 <div class="consultation-text">
                     <p class="consultation-label">상담신청</p>
                     <h2>CONSULTATION REQUEST</h2>
-                    <p class="consultation-subtitle">법률 상담부터 해결까지 파노 법률사무소가 도와드립니다.</p>
+                    <p class="consultation-subtitle">법률 상담부터 해결까지<br class="mobile-br"> 파노 법률사무소가 도와드립니다.</p>
                 </div>
                 
                 <form id="consultationForm" class="consultation-form" method="POST" action="/api/submit_consultation.php">
