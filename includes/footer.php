519f04b3936e51cd77c79a62a75fab4ca11581a4
kimsungtaek1 <kstbook@naver.com>
2025-11-21 21:10:52 +0900

    모바일: 푸터 정보 5줄로 줄바꿈 적용 (PC는 기존 형식 유지)

--- includes/footer.php
+++ includes/footer.php
@@ -36,7 +36,9 @@
                 <div class="footer-right">
                     <div class="footer-info">
                         <p>파노 법률사무소 | 광고책임변호사 송동민</p>
-                        <p>주소: 서울 서초구 반포대로28길 63, 3층 | 대표번호: 02-1551-8385 | 팩스번호: 02-6008-2884 | 대표이메일: intake@panolaw.com</p>
+                        <p class="footer-address">주소: 서울 서초구 반포대로28길 63, 3층</p>
+                        <p class="footer-contact">대표번호: 02-1551-8385 | 팩스번호: 02-6008-2884</p>
+                        <p class="footer-email">대표이메일: intake@panolaw.com</p>
                         <p>© Copyright 2025 Law Firm PANO. All Rights Reserved.</p>
                     </div>
                     <div class="footer-links">
