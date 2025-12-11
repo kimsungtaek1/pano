806cf537b82a67d7ed5eb84f647c80feb97d28a5
kimsungtaek1 <kstbook@naver.com>
2025-12-11 12:55:54 +0900

    OG description에서 '40년 전통의 법률사무소' 문구 삭제

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <!-- Open Graph 메타 태그 (카카오톡, 페이스북 등 SNS 공유용) -->
     <meta property="og:type" content="website">
     <meta property="og:title" content="PANO - 법률사무소">
-    <meta property="og:description" content="의뢰인의 믿음과 신뢰를 받을 수 있도록 최선을 다하겠습니다. 40년 전통의 법률사무소">
+    <meta property="og:description" content="의뢰인의 믿음과 신뢰를 받을 수 있도록 최선을 다하겠습니다.">
     <meta property="og:image" content="https://panolawyer.com/images/thumbnail.png">
     <meta property="og:url" content="https://panolawyer.com">
 
