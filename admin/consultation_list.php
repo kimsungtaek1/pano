72beaeae97a7a736c26e3e5503db9c02547b606f
kimsungtaek1 <kstbook@naver.com>
2026-02-20 18:47:45 +0900

    auto: update files

--- admin/consultation_list.php
+++ admin/consultation_list.php
@@ -145,6 +145,9 @@ try {
                 <a href="consultation_list.php" class="active">
                     <span class="nav-icon">💬</span> 상담신청 관리
                 </a>
+                <a href="landing_list.php">
+                    <span class="nav-icon">🚀</span> 랜딩페이지
+                </a>
                 <a href="news_list.php">
                     <span class="nav-icon">📰</span> 뉴스 관리
                 </a>
